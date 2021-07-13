d4e03e87e0c6dcb0e215fad8e5f9f9c9724f1152
SHRISANKAR <85781471+SHRISANKAR@users.noreply.github.com>
2021-07-13 14:04:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from "react"
+import "./style.css"
+import Button from "./Button"
+
+const App =()=>{
+    return(
+        <div>
+             <h1 style={{textAlign : "center"}}> HELLO WORLD </h1>
+             <h2 style={{textAlign : "center"}}>HELLO CLASS MATES</h2>
+             <Button buttontext="hey"/>
+             <Button buttontext="hai"/>
+             <Button buttontext="hello"/>
+             <Button buttontext="namaste"/>
+             <Button buttontext="vanakam"/>
+             <Button buttontext="call me"/>
+             <Button buttontext="home"/>
+             <Button buttontext="about"/>
+             <Button buttontext="hai"/>
+             <Button buttontext="bye"/>
+            
+        </div>
+      
+    )
+}
+
+export default App
---
+++ Button.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Button = ({buttontext})=>{
+    return(
+        
+        <button className="button">{buttontext}</button>
+    )
+}
+export default Button
+
